5224441d39f8f3f076c0a88932cbe300f0ac32d6
Viktorrrrr <viktorcvetanovic@gmail.com>
2021-11-04 15:18:26 +0100

    initial commit

---
+++ backend/src/main/java/com/viktor/skype/SkypeApplication.java
@@ -0,0 +1,13 @@
+package com.viktor.skype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkypeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/viktor/skype/SkypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viktor.skype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
